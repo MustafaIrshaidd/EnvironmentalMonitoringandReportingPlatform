f9cb93d91b5cedf5774d12e1d96c089cdbe7646b
Mohammad Omar <mohammad.m.omar.01@gmail.com>
2023-12-22 23:01:31 +0200

    DataCollection Apis
    [200~feat: Add dataCollection routes and controller methods

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import databaseConnection from "./services/DatabaseConnection.js";
 import userRouter from "./routes/user.route.js";
+import dataCollectionRouter from "./routes/dataCollection.router.js";
 
 // Create an Express app
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json());
 
 // Routes
 app.use(`/users`, userRouter);
+app.use(`/dataCollection`, dataCollectionRouter);
 
 // Start the Express server
 app.listen(3000, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 import userRouter from "./user.route";
+import dataCollectionRouter from "./dataCollection.router";
 
 export default {
   userRouter,
+  dataCollectionRouter
 };
