d7dea229103689b438128ba2f0cdea46d163c390
ahmadamira <103905674+ahmadamira@users.noreply.github.com>
2023-12-23 14:45:21 +0200

    Finished with the Educational resources feature

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import databaseConnection from "./services/DatabaseConnection.js";
 import userRouter from "./routes/user.route.js";
+import resourceRouter from "./routes/educationalResources.route.js";
 
 // Create an Express app
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json());
 
 // Routes
 app.use(`/users`, userRouter);
+app.use(`/resources`, resourceRouter);
 
 // Start the Express server
 app.listen(3000, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 import userRouter from "./user.route";
+import resourceRouter from "./educationalResources.route";
 
 export default {
   userRouter,
+  resourceRouter,
 };
